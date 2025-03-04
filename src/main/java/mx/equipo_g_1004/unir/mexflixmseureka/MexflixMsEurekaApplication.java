3d8cfa0c9d4f9a42904558504ab6c3bbb0f60143
AgnosticSix <mateo_godfather@hotmail.com>
2025-03-03 23:17:17 -0600

    initialize mexflix-ms-eureka project with basic structure and configuration

---
+++ src/main/java/mx/equipo_g_1004/unir/mexflixmseureka/MexflixMsEurekaApplication.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexflixMsEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MexflixMsEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/equipo_g_1004/unir/mexflixmseureka/MexflixMsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MexflixMsEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
