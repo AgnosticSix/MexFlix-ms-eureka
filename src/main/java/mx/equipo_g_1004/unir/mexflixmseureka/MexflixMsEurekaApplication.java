fb9e8a8ca24ab0ec62cdaa92959a69404f471fbb
AgnosticSix <mateo_godfather@hotmail.com>
2025-03-03 23:20:11 -0600

    configure Eureka server settings and update application properties

--- src/main/java/mx/equipo_g_1004/unir/mexflixmseureka/MexflixMsEurekaApplication.java
+++ src/main/java/mx/equipo_g_1004/unir/mexflixmseureka/MexflixMsEurekaApplication.java
@@ -2,8 +2,10 @@ package mx.equipo_g_1004.unir.mexflixmseureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MexflixMsEurekaApplication {
 
     public static void main(String[] args) {
